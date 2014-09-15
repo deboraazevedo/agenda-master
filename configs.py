5de89a81e541771e3ecc78a8808b3d92f0c50ae3
Debora Azevedo <deboraazevedoo@gmail.com>
2014-09-15 00:36:47 -0300

    Deleting the .pyc files and securing databe information

--- configs.py
+++ configs.py
@@ -1,2 +1,4 @@
 SECRET_KEY = 'you-will-never-guess'
-MONGO_URI = "mongodb://debora:debora123@ds029117.mongolab.com:29117/newagenda"
+MONGO_URI = "mongodb://db_username:db_username_passwd@provided_by.mongolab.com:provided_by_mongolab/collection_name"
+
+#Provided by mongo lab refers to the information MongoLAB datebase provides you to use this database
